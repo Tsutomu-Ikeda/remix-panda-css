9b4703f93c7979cbe6f74a6af6e5516206bca687
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2024-09-05 21:36:58 +0900

    Add jsx styled button

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -7,7 +7,20 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import styles from "./index.css?url"
+import { styled } from "@styled-system/jsx";
 import "./tailwind.css";
+
+const Button = styled("button", {
+  base: {
+    fontSize: "1rem",
+    padding: "0.5rem 1rem",
+    border: "none",
+    borderRadius: "0.25rem",
+    color: "white",
+    backgroundColor: "blue.600",
+    cursor: "pointer",
+  }
+})
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -20,6 +33,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Button>
+          ボタン
+        </Button>
         <ScrollRestoration />
         <Scripts />
       </body>
